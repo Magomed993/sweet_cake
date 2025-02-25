bcbf92769271b0e79d143f53b79555241234a6c8
Magomed993 <160238040+Magomed993@users.noreply.github.com>
2025-02-25 23:57:40 +0300

    Initial commit

---
+++ BakeCake/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from cake.views import index, lk, lk_order
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", index, name="index"),
+    path("lk", lk, name="lk"),
+    path("lk-order", lk_order, name="lk-order"),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
