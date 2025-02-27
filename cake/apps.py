0b7b87f545958c4a0ebef2b55a96cead537dc838
sibeardev <82308935+sibeardev@users.noreply.github.com>
2025-02-27 10:27:48 +0300

    feat: добавляет создание клиента при создании пользователя

--- cake/apps.py
+++ cake/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class CakeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cake'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cake"
+
+    def ready(self):
+        import cake.signals
