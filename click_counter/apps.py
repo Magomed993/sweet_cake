db572ae2bb13a909fc5af2b5c32230146c82137b
sibeardev <82308935+sibeardev@users.noreply.github.com>
2025-03-01 11:18:57 +0300

    fix: исправляет django.db.utils.OperationalError: no such table: background_task при применении миграций

--- click_counter/apps.py
+++ click_counter/apps.py
@@ -1,4 +1,8 @@
+import logging
+
 from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
 
 
 class ClickCounterConfig(AppConfig):
@@ -6,5 +10,12 @@ class ClickCounterConfig(AppConfig):
     name = "click_counter"
 
     def ready(self):
+        from django.db.utils import OperationalError, ProgrammingError
+
         from click_counter.tasks import update_click_counts
-        update_click_counts()
+
+        try:
+            update_click_counts()
+        except (OperationalError, ProgrammingError) as e:
+            logger.warning(f"Не удалось запустить update_click_counts: {e}")
+
